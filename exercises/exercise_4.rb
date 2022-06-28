98f5e647a1500dc00b413984479cb985b2719e23
Ali <alihbouran@gmail.com>
2022-06-28 11:16:29 -0700

    Finish exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -8,14 +8,15 @@ puts "----------"
 
 # Your code goes here ...
 surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
-whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: false, womens_apparel: false)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
-@mens_stores = Store.where(:mens_apparel => true, :womens_apparel => false)
-puts @mens_stores
+# @mens_stores = Store.where(["mens_apparel = ? AND womens_apparel = ?", true, false])
+@mens_stores = Store.where(["mens_apparel = ? and womens_apparel = ?", true, false])
+
+puts @men_stores
 
 @mens_stores.each do |men_store|
   puts "Name: #{men_store.name}"
   puts "Annual Revenue: #{men_store.annual_revenue}"
 end
-
