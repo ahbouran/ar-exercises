df578c04218beaa8d4fc6c132692d612592a3a56
Ali <alihbouran@gmail.com>
2022-06-28 10:54:44 -0700

    Want to refactor

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -7,7 +7,9 @@ puts "----------"
 # Your code goes here ...
 store1 = Store.find_by(id: 1)
 @store1 = store1
+puts @store1.name
 store2 = Store.find_by(id: 2)
 @store2 = store2
 
 @store1.name = "Coquitlam"
+@store1.save
